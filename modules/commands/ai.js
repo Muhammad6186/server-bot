7f8fdc33facd6b7b4a3697a943609327888934f1
Muhammad6186 <90974141+Muhammad6186@users.noreply.github.com>
2024-05-10 11:03:09 +0000

    Initial commit

---
+++ modules/commands/ai.js
@@ -0,0 +1,44 @@
+module.exports.config = {
+		name: "ai",
+		version: "1.0.0",
+		role: 0,
+		credits: "cliff",
+		hasPrefix: false,
+		description: "This module provides AI-powered responses using GPT-3.",
+		usage: "<question>",
+		cooldowns: 5,
+	usePrefix: false,
+	  hasPermission: 0,
+	  usages: "<question>",
+		cooldown: 5,
+	 commandCategory: "gpt4",
+		aliases: ["Gpt3"]
+};
+
+module.exports.run = async function ({ api, event, args, _GLOBAL, client}) {
+		try {
+				const { G4F } = require("g4f");
+
+				function reply(a) {
+						api.sendMessage(a, event.threadID, event.messageID);
+				}
+
+				const g4f = new G4F();
+				const textInput = args.join(' ');
+				if (!textInput) return reply('Please provide a question.');
+
+				const messages = [
+						{ role: "user", content: textInput }
+				];
+				const options = {
+						provider: g4f.providers.GPT,
+						model: "gpt-3.5-turbo",
+						debug: true,
+						proxy: `${global.config.UPDATE}`
+				};
+				const response = await g4f.chatCompletion(messages, options);
+				reply(response);
+		} catch (e) {
+				return reply(e.message);
+		}
+}
